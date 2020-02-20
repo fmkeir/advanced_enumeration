b0b79c67c80b3cf84185c317399c032a2886e983
FKeir <fmkeir@gmail.com>
2020-02-20 09:44:02 +0000

    Create an anagram finder

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,11 @@
 const AnagramFinder = function (word) {
-
+  this.word = word;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+  return otherWords.filter(otherWord => {
+    return (otherWord.toLowerCase().split('').sort().join('')) === (this.word.toLowerCase().split('').sort().join('')) && otherWord !== this.word;
+  });
 }
 
 module.exports = AnagramFinder;
